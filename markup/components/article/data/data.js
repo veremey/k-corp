338bdb54c233269bb41216f5a90e8bfac5f05b64
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-06-27 10:16:39 +0300

    #1

---
+++ markup/components/article/data/data.js
@@ -0,0 +1,142 @@
+var data = {article: {
+	about: [{
+		title: 'Привет, я Дима Ковпак',
+		footerText: 'Я решил помочь 100,000 людям <br>создать свой бизнес с Китаем',
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel</p><p> dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		video: '!0',
+		more: 'Узнать подробнее'
+	}],
+	blog: [{
+		mod: 'article_left',
+		title: 'Lorem ipsum dolor <br>sit amet',
+		time:
+			{
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: '!1',
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel</p><p> dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью'
+	},{
+		mod: '',
+		title: 'Lorem ipsum dolor <br>sit amet',
+		time:
+			{
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: '!1',
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel</p><p> dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью'
+	},{
+		mod: 'article_left',
+		title: 'Lorem ipsum dolor <br>sit amet',
+		time:
+			{
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: '!1',
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel</p><p> dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью'
+	}],
+	blogItem: [{
+		mod: 'article_blog  article_left',
+		title: 'Привет, я Дима Ковпак',
+		time: {
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: [{
+					tagLink: '#',
+					tagName: 'Tag1,'
+				},{
+					tagLink: '#',
+					tagName: 'Tag2'
+				}],
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью',
+		share: '!0',
+		comment: 'pi'
+	}, {
+		mod: 'article_blog',
+		title: 'Привет, я Дима Ковпак',
+		time: {
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: [{
+					tagLink: '#',
+					tagName: 'Tag1,'
+				},{
+					tagLink: '#',
+					tagName: 'Tag2'
+				}],
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью',
+		share: '!0',
+		comment: 'pi1'
+	}, {
+		mod: 'article_blog article_left',
+		title: 'Привет, я Дима Ковпак',
+		time: {
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: [{
+					tagLink: '#',
+					tagName: 'Tag1,'
+				},{
+					tagLink: '#',
+					tagName: 'Tag2'
+				}],
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью',
+		share: '!0',
+		comment: 'pi2'
+	}, {
+		mod: 'article_blog',
+		title: 'Привет, я Дима Ковпак',
+		time: {
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: [{
+					tagLink: '#',
+					tagName: 'Tag1,'
+				},{
+					tagLink: '#',
+					tagName: 'Tag2'
+				}],
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью',
+		share: '!0',
+		comment: 'pi3'
+	}, {
+		mod: 'article_blog article_left',
+		title: 'Привет, я Дима Ковпак',
+		time: {
+				date: '03.10.2019',
+				read: '2 минуты на прочтение'
+			},
+		tag: [{
+					tagLink: '#',
+					tagName: 'Tag1,'
+				},{
+					tagLink: '#',
+					tagName: 'Tag2'
+				}],
+		desc: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos eius aliquid nihil atus vel dignissimos nemo, ea excepturi delectus ipsam, consectetur nobis autem quia cupiditate. Nam ad corrupti nisi, aperiam, quam beatae dolorum commodi. Blanditiis ut similique quibusdam eveniet culpa nisi, quia, non! Dolore, ex ipsum sint cumque similique magni fugit possimus libero sapiente modi beatae consequatur unde rerum odit iste vel. Iste ratione nam, iusto ipsa.</p>',
+		photo: 'bayer',
+		more: 'Читать полностью',
+		share: '!0',
+		comment: 'pi4'
+	}]
+}};
